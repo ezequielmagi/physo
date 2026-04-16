a9d12c7ca1991c49b3bb8922009c7d9f3dd97b77
Ezequiel <ezequielmaggi@gmail.com>
2026-04-16 00:39:01 -0300

    Agreggo Footer

--- footer.php
+++ footer.php
@@ -6,32 +6,39 @@
     <div class="site-footer__grid">
 
       <div class="site-footer__col">
-        <div class="site-footer__logo">physo</div>
-        <p class="site-footer__desc">
-          Centro de acompañamiento integral en movimiento, salud y bienestar.
-        </p>
+        <div class="site-footer__logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.png" alt="Physo Logo">
+        </div>
+        <div class="site-footer__desc-container">
+          <p class="site-footer__desc">
+            Centro de acompañamiento integral en movimiento, salud y bienestar.
+          </p>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/down-arrow.png" alt="Flecha hacia abajo">
+        </div>
         <div class="physo-social">
-          <a href="#" aria-label="Facebook">f</a>
-          <a href="#" aria-label="Instagram">ig</a>
+          <a href="#" aria-label="Facebook"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt=""></a>
+          <a href="#" aria-label="Instagram"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt=""></a>
         </div>
       </div>
 
       <div class="site-footer__col">
         <h4 class="site-footer__heading">Navegación</h4>
-        <nav class="site-footer__nav">
-          <?php wp_nav_menu( [
-            'theme_location' => 'footer',
-            'container'      => false,
-            'fallback_cb'    => false,
-          ] ); ?>
-        </nav>
+        <ul class="site-footer__nav">
+          <li><a href="<?php echo esc_url( home_url('/') ); ?>">Movimiento Adaptado</a></li>
+          <li><a href="<?php echo esc_url( home_url('/nosotros') ); ?>">Readaptación funcional</a></li>
+          <li><a href="<?php echo esc_url( home_url('/servicios') ); ?>">Yoga nidra</a></li>
+          <li><a href="<?php echo esc_url( home_url('/contacto') ); ?>">Metodología</a></li>
+          <li><a href="<?php echo esc_url( home_url('/contacto') ); ?>">Sobre Nosotros</a></li>
+          <li><a href="<?php echo esc_url( home_url('/contacto') ); ?>">Blog</a></li>
+        </ul>
       </div>
 
       <div class="site-footer__col">
         <h4 class="site-footer__heading">Contacto</h4>
         <div class="site-footer__contact">
           <p>contacto@physo.com</p>
-          <p>+54 9 11 0000-0000</p>
+          <p>(414) 687 - 5892</p>
+          <p>Jirón Cruz del Sur #140,<br>Int. 308 (Edificio Time) - Surco</p>
         </div>
       </div>
 
